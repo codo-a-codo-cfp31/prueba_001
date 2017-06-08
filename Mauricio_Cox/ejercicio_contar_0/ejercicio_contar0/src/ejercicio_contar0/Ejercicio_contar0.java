2ceb9ff4c2ca58a5f6f14c5550a11f4efa75ad14
Mauricio Cox <ososfull@gmail.com>
2017-06-08 20:49:50 -0300

    ggzzfzfzf

---
+++ Mauricio_Cox/ejercicio_contar_0/ejercicio_contar0/src/ejercicio_contar0/Ejercicio_contar0.java
@@ -0,0 +1,12 @@
+package ejercicio_contar0;
+public class Ejercicio_contar0 {
+    public static void main(String[] args) {
+        
+        //Ejercicio contar hasta cero
+        
+        int contar = 0;
+        
+        
+    }
+    
+}
