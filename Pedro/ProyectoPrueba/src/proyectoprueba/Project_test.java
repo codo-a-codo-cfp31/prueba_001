5316f730161a1913ddfe38708e66f8b540003215
DESKTOP-N4TUM8U\CFP31-09 <tineopedro@hotmail.com>
2017-06-08 21:12:18 -0300

    Programa que muestra de forma descendente desde X numero hasta 0

---
+++ Pedro/ProyectoPrueba/src/proyectoprueba/Project_test.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoprueba;
+import java.util.*;
+/**
+ *
+ * @author CFP31-08
+ */
+public class Project_test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {//las llaves son el inicio y el fin de las funciones y algoritmo
+        // TODO code application logic here
+        int contador = 0;// se define el tipo de dato y se le asignan valores
+        int numero_para_contar;
+        
+        Scanner un_scanner = new Scanner(System.in);//para leer se crea el objeto con new, system.in es para que imprima en el cuadrito
+                
+        numero_para_contar = un_scanner.nextInt();//convierte lo escaneado a entero con el nextInt
+        
+        while (contador<numero_para_contar) {//en while la accion debe estar entre parentesis
+            contador = contador+1;
+        System.out.println(contador);//imprimir
+        }
+    }
+}
