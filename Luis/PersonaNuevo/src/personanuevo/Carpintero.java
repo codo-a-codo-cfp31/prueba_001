481dcfdc7c016a77f7df594b9e24a1a7532ee8fd
Luis Vera <vera.luisangel@gmail.com>
2017-06-25 21:56:57 -0300

    Persona "Nuevo"

---
+++ Luis/PersonaNuevo/src/personanuevo/Carpintero.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personanuevo;
+
+/**
+ *
+ * @author Luis Vera
+ */
+public class Carpintero extends Persona {
+    
+    public Carpintero (String unNombre, String unApellido, int unaEdad){
+        super (unNombre, unApellido, unaEdad);
+    
+}
+    
+}
