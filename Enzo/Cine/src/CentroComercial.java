ab1921dd6b978dc305e9305b589012bafe7e1298
DESKTOP-SMJBG3B\CFP31-03 <darioenzojavier@gmail.com>
2017-06-27 20:23:33 -0300

    Actualizano la carpeta

---
+++ Enzo/Cine/src/CentroComercial.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author ELSA.BENAviD00953
+ */
+public class CentroComercial {
+    protected String nombre;
+    protected String direccion;
+    protected String horario;
+    protected SalaCine susSalasDeCine;
+    
+    public CentroComercial (String unNombre, String unaDireccion, String unHorario){
+            nombre = unNombre;
+            direccion = unaDireccion;
+            horario = unHorario;
+}   
+    public String nombreDeCentroComercial(){
+        return nombre;
+}   
+    public String direccionDeCentroComercial(){
+        return direccion;
+}
+    public String horarioDeCentroComercial(){
+        return horario;
+    }
+}  
---
+++ Enzo/Mascota/src/mascota/Mascota.java
@@ -0,0 +1,61 @@
+package mascota;
+import java.io.*;
+
+public class Mascota {
+
+	public static void imprimirmenu() {
+		System.out.println("***********************************************************************");
+		System.out.println(" Seleccione una de las opciones de acuerdo a lo que más haga su mascota");
+		System.out.println("***********************************************************************");
+		System.out.println(" 1- Ladrar");
+		System.out.println(" 2- Saltar");
+		System.out.println(" 3- Maullar");
+		System.out.println(" 4- Ayuda");
+		System.out.println(" 0- Salir del programa");
+                System.out.println("***********************************************************************");
+                System.out.println("***********************************************************************");
+	}
+
+	public static void main(String args[]) throws IOException {
+		BufferedReader bufEntrada = new BufferedReader(new InputStreamReader(System.in));
+		
+		String acciones[];
+		int opcion;
+		imprimirmenu();
+              
+		acciones = new String[5];
+		acciones[1] = "Ladrar";
+		acciones[2] = "Saltar";
+		acciones[3] = "Maullar";
+		acciones[4] = "Ayuda";
+		acciones[0] = "Salir";
+		do {
+			opcion = 0;
+			opcion = Integer.parseInt(bufEntrada.readLine());
+                System.out.println("***********************************************************************");
+		System.out.println("Opcion seleccionada es: "+opcion);
+		System.out.println("***********************************************************************");
+			if ((opcion)==(1)) {
+				System.out.println("La Mascota es un Perro");
+			}else if ((opcion)==(2)) {
+				System.out.println("La Mascota es un Conejo");
+			}else if ((opcion)==(3)) {
+				System.out.println("La Mascota es un Gato");
+			}else if ((opcion)==(4)) {
+                                System.out.println("***********************************************************************");
+				System.out.println("Debe seleccionar accion de su mascota");
+                                System.out.println(" 1- Ladrar");
+                                System.out.println(" 2- Saltar");
+                                System.out.println(" 3- Maullar");
+                                System.out.println(" 4- Ayuda");
+                                System.out.println(" 0- Salir del programa");
+                                System.out.println("***********************************************************************");
+			}else if ((opcion)==(0)) {
+				System.out.println("A sido un gusto que uses este programa");
+			}else{ 
+                         System.out.println("**************************************");
+                         System.out.println("Error de seleccion e vuelva intentarlo");
+                         System.out.println("**************************************");}
+		} while ((opcion)!=(0));
+            }
+        }
