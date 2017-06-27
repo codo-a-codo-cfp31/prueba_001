eb2c7924940a59aa5d2ba2ca779afd0db8d4580a
Alejandro Medici <el.aleyo@gmail.com>
2017-06-27 20:54:14 -0300

    Agregado POO2 y 3 + Extraterrestre e IPersona

---
+++ Ale/POO2/src/poo2/POO2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo2;
+
+/**
+ *
+ * @author CFP31-15
+ */
+public class POO2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Cuadrupedo unPerro = new Perro();
+        
+        unPerro.Trotar();
+        //unPerro.Ladrar();
+        
+        String unTexto = "lalalal";
+        
+       
+    }
+    
+}
