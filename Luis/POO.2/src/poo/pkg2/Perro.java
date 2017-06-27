1fe1e7107e49596fbc96b08da5fcc8f2054a060b
Ju <deyulai@gmail.com>
2017-06-27 20:51:04 -0300

    no message

---
+++ Luis/POO.2/src/poo/pkg2/Perro.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo.pkg2;
+
+/**
+ *
+ * @author cfp31-06
+ */
+public class Perro extends Mamiferos implements Cuadrupedos{
+    
+    public void Trotar(){
+        
+    }
+    
+    public void Moverse(){
+        
+    }
+    
+    public void COrrer(){
+        
+    }
+    
+}
