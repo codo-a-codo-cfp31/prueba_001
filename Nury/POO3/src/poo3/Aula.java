acf1d1f9870f4772a5840a219cb7a2a299e39c3e
DESKTOP-PEJUM3O\CFP31-08 <nurycardenas7@gmail.com>
2017-06-27 21:23:59 -0300

    Ejercicio de animales personas y aula

---
+++ Nury/POO3/src/poo3/Aula.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo3;
+import java.util.*;
+
+/**
+ *
+ * @author CFP31-08
+ */
+public class Aula {
+    // aca creo una lista de personas y una de muebles
+    protected List<Persona> lista_de_personas;
+    protected List<Mueble> lista_de_muebles;
+    
+    //este es mi constructor.
+    public Aula(){
+    Alumno unAlumno = new Alumno(); 
+    Profesor unProfesor = new Profesor();
+    
+}
+}
