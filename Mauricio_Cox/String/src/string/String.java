fc251ed482f0a6dbbf359fd377a737fd15a7462d
Mauricio Cox <ososfull@gmail.com>
2017-06-29 19:11:27 -0300

    jsjahaa

---
+++ Mauricio_Cox/String/src/string/String.java
@@ -0,0 +1,22 @@
+/*
+crear un algoritmo el "semicurso" que permita recorrer un vector de notas de 20 posiciones y que termine:
+A. Nota más alta y nota más baja de cuuurso
+B. Promedio de las notas
+c. Cantidad de notas superior al promedio
+D. Cantidad de alumnos aprobados y desaprobados (se aprueba con 6)mostrar en pantalla si hay 2 notas o mas notas consecutivas
+E. 
+F. los nombres de los alumnos vienen todos juntos en un String separados por un punto y coma, guardarlos en un vector por su orden 
+correspondientes indicar que alumnos tienen la mayor nota*/
+package string;
+
+public class String {
+
+    public static void main(String arg[ ]) {
+ 
+          int[] numerodenotas = new int[20];
+          int i;
+ 
+          System.out.print("El numero de notas ");
+    }
+ 
+}
