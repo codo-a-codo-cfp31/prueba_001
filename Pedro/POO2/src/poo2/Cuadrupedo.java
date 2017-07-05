1136db102230c5c1bcfea7360059d417520900da
DESKTOP-N4TUM8U\CFP31-09 <tineopedro@hotmail.com>
2017-07-04 21:35:53 -0300

    lista

---
+++ Pedro/POO2/src/poo2/Cuadrupedo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo2;
+
+/**
+ *
+ * @author CFP31-08
+ */
+// esto es una interface es un espacio donde puedo defenir el comportamiento de un objeto o varios obejtos
+//ademas sirve para heredar comportamientos de dos clases diferentes ya que java solo permite heredar de una
+public interface Cuadrupedo {
+    public void Correr();
+    public void Moverse();
+    public void Trotar();
+}
---
+++ Pedro/POO3/src/poo3/Persona.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo3;
+
+/**
+ *
+ * @author CFP31-15
+ */
+public class Persona implements IPersona {
+     private String nombre;
+     private String apellido;
+     private int edad;
+     protected int dni;
+    
+     public String comoTeLlamas(){//le pregunto como se llama y me retorna su nombre esto es un metodo
+         return nombre;
+     }
+     
+     public void ahoraTeLlamas(String tuNuevoNombre){//le asigno el nombre que yo deseo.
+         nombre = tuNuevoNombre;
+     }
+     
+     public String cualEsTuApellido(){
+         return apellido;
+     }
+     
+     public void ahoraTuApellido(String tuNuevoApellido){
+         apellido = tuNuevoApellido;
+     }
+     
+     public int cualEsTuEdad(){
+         return edad;
+     }
+     
+     public void ahoraTuEdadEs(int tuNuevaEdad){
+         edad = tuNuevaEdad;
+     }
+     
+     public int cualEsTuDNI(){
+         return dni;
+     }
+     
+     public void ahoraTuDNIEs(int tuNuevoDNI){
+         dni = tuNuevoDNI;
+     }
+     // un contructor que no tiene parametros o argumentos
+    public Persona(){
+     }
+
+}     
