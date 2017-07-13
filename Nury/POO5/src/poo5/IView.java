17d5fdfa449e5f075466cc1e3abadae3712e3562
Nury Cardenas <nurycardenas7@gmail.com>
2017-07-13 19:38:03 -0300

    clase 13-7.17

---
+++ Nury/POO5/src/poo5/IView.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo5;
+
+/**
+ *
+ * @author CFP31-21
+ */
+public interface IView {
+    
+    public void Draw(String texto);
+    public void Clear();
+    
+}
