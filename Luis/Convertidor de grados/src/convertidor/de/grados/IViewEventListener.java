88b67c95b75d0451fbdc136b950b64924b229919
Luis <vera.luisangel@gmail.com>
2017-08-31 21:25:04 -0300

    Convertidor de C a F

---
+++ Luis/Convertidor de grados/src/convertidor/de/grados/IViewEventListener.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package convertidor.de.grados;
+
+import java.awt.Event;/**
+ *
+ * @author cfp31-06
+ */
+public interface IViewEventListener {
+    public void listen(Event event);
+    
+}
