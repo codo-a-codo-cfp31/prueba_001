d3f44cab3efd01edac8a5d65b843c6fabd415574
alumno <alumno@DESKTOP-N2H88CC>
2017-09-26 21:21:27 -0400

    Calculadora

---
+++ Ale/Calculadora/src/calculadora/ICalculadora.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+import java.util.List;
+
+/**
+ *
+ * @author CFP31-15
+ */
+public interface ICalculadora {
+
+    public void binaryOperation(float elemento1, String operacion, float elemento2);
+}
