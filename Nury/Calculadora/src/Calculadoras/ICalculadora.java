0e1a27c0f4cce8e9164ddedfb3986c6497973401
Nury Cardenas <nurycardenas7@gmail.com>
2017-09-26 21:24:55 -0300

    calculadora

---
+++ Nury/Calculadora/src/Calculadoras/ICalculadora.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Calculadoras;
+
+//import java.util.List;
+
+/**
+ *
+ * @author CFP31-10
+ */
+public interface ICalculadora {
+    
+    
+    public void BinaryOperation(float elemento1,String Operador, float elemento2);
+    public float Result();
+   // public List<String>getUnitList();
+
+}
