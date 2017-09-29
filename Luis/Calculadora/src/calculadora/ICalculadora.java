07abffb82bb5b5dc62210f06006bf1a192eb0af0
Luis <vera.luisangel@gmail.com>
2017-09-28 21:30:41 -0300

    Calculadora simple

---
+++ Luis/Calculadora/src/calculadora/ICalculadora.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+import java.util.List;
+
+/**
+ *
+ * @author CFP31-15
+ */
+public interface ICalculadora {
+
+    public void binaryOperation(float elemento1, String operacion, float elemento2);
+    
+    public float getResultado();
+}
