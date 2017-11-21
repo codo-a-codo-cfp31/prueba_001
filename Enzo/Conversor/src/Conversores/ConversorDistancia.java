9da17579bc3127efa8e40e9c0d74b2bb815f7890
Enzo <darioenzojavier@gmail.com>
2017-11-21 19:41:05 -0300

    Actualizando Conversor

--- Enzo/Conversor/src/Conversores/ConversorDistancia.java
+++ Enzo/Conversor/src/Conversores/ConversorDistancia.java
@@ -7,133 +7,111 @@ package Conversores;
 
 import conversor.*;
 import java.util.*;
+
 /**
  *
  * @author CFP31-15
  */
 public class ConversorDistancia implements IConversor {
-  protected float distance;
+
+    protected float distance;
     protected String unit;
 
     public ConversorDistancia() {
-        distance=0.0f;
-        unit="";
+        distance = 0.0f;
+        unit = "";
     }
 
     @Override
     public void setMedida(float newDistance, String newUnit) {
-        distance=newDistance;
-        unit=newUnit;
+        distance = newDistance;
+        unit = newUnit;
     }
 
     @Override
     public float getMedida(String toUnit) {
-        float distanceResult=distance;
-         if (unit.equalsIgnoreCase("mi")) {
+        float distanceResult = distance;
+        if (unit.equalsIgnoreCase("mi")) {
             if (toUnit.equalsIgnoreCase("km")) {
-                distanceResult=distance*(1609344f/1000000f);
+                distanceResult = distance * (1609344f / 1000000f);
             } else {
                 //de C a C no hago Nada de Nada
             }
-          if (toUnit.equalsIgnoreCase("m")) {
-                distanceResult=distance*1609.344f;
-            } else {
-                //de C a C no hago Nada de Nada
-            }
-           if (toUnit.equalsIgnoreCase("cm")) {
-                distanceResult=distance*1609.344f*100f;
-            } else {
-                //de C a C no hago Nada de Nada
-            }
-        } else {
-            if (toUnit.equalsIgnoreCase("mi")) {
-                distanceResult=distance/(1609.344f/1000f);
-            } else {
-                //de K a K No hago nada de nada
-            }
- }
-         
-        if (unit.equalsIgnoreCase("km")) {
-           if (toUnit.equalsIgnoreCase("mi")) {
-                distanceResult=distance*(1000/1609.344f);
-            } else {
-                //de C a C no hago Nada de Nada
-            }
-           if (toUnit.equalsIgnoreCase("m")) {
-                distanceResult=distance*1000f;
-            } else {
-                //de C a C no hago Nada de Nada
-            }
-          if (toUnit.equalsIgnoreCase("cm")) {
-                distanceResult=distance*100000f;
-            } else {
-                //de C a C no hago Nada de Nada
-            }
-        } else {
-            if (toUnit.equalsIgnoreCase("km")) {
-                distanceResult=distance/(1000/1609.344f);
-            } else {
-                //de K a K No hago nada de nada
-            }
- }
-        
-         if (unit.equalsIgnoreCase("m")) {
-           if (toUnit.equalsIgnoreCase("mi")) {
-                distanceResult=distance*(1/1609.344f);
-            } else {
-                //de C a C no hago Nada de Nada
-            }
-           if (toUnit.equalsIgnoreCase("km")) {
-                distanceResult=distance/1000f;
-            } else {
-                //de C a C no hago Nada de Nada
-            }
-          if (toUnit.equalsIgnoreCase("cm")) {
-                distanceResult=distance*100f;
-            } else {
-                //de C a C no hago Nada de Nada
-            }
-        } else {
             if (toUnit.equalsIgnoreCase("m")) {
-                distanceResult=distance/(1/1609.344f);
-            } else {
-                //de K a K No hago nada de nada
-            }
- }
-         
-         if (unit.equalsIgnoreCase("cm")) {
-           if (toUnit.equalsIgnoreCase("mi")) {
-                distanceResult=distance*1/(1609.344f*100f);
+                distanceResult = distance * 1609.344f;
             } else {
                 //de C a C no hago Nada de Nada
             }
-           if (toUnit.equalsIgnoreCase("km")) {
-                distanceResult=distance/100000f;
-            } else {
-                //de C a C no hago Nada de Nada
-            }
-           if (toUnit.equalsIgnoreCase("m")) {
-                distanceResult=distance/100f;
-            } else {
-                //de C a C no hago Nada de Nada
-            }
-        } else {
             if (toUnit.equalsIgnoreCase("cm")) {
-                distanceResult=distance/(1/(1609.344f*100f));
+                distanceResult = distance * 1609.344f * 100f;
             } else {
-                //de K a K No hago nada de nada
+                //de C a C no hago Nada de Nada
             }
- }
- return distanceResult;
+        }
+
+        if (unit.equalsIgnoreCase("km")) {
+            if (toUnit.equalsIgnoreCase("mi")) {
+                distanceResult = distance * (1000 / 1609.344f);
+            } else {
+                //de C a C no hago Nada de Nada
+            }
+            if (toUnit.equalsIgnoreCase("m")) {
+                distanceResult = distance * 1000f;
+            } else {
+                //de C a C no hago Nada de Nada
+            }
+            if (toUnit.equalsIgnoreCase("cm")) {
+                distanceResult = distance * 100000f;
+            } else {
+                //de C a C no hago Nada de Nada
+            }
+        }
+
+        if (unit.equalsIgnoreCase("m")) {
+            if (toUnit.equalsIgnoreCase("mi")) {
+                distanceResult = distance * (1 / 1609.344f);
+            } else {
+                //de C a C no hago Nada de Nada
+            }
+            if (toUnit.equalsIgnoreCase("km")) {
+                distanceResult = distance / 1000f;
+            } else {
+                //de C a C no hago Nada de Nada
+            }
+            if (toUnit.equalsIgnoreCase("cm")) {
+                distanceResult = distance * 100f;
+            } else {
+                //de C a C no hago Nada de Nada
+            }
+        }
+
+        if (unit.equalsIgnoreCase("cm")) {
+            if (toUnit.equalsIgnoreCase("mi")) {
+                distanceResult = distance * 1 / (1609.344f * 100f);
+            } else {
+                //de C a C no hago Nada de Nada
+            }
+            if (toUnit.equalsIgnoreCase("km")) {
+                distanceResult = distance / 100000f;
+            } else {
+                //de C a C no hago Nada de Nada
+            }
+            if (toUnit.equalsIgnoreCase("m")) {
+                distanceResult = distance / 100f;
+            } else {
+                //de C a C no hago Nada de Nada
+            }
+        }
+        return distanceResult;
     }
 
     @Override
     public List<String> getUnitList() {
-       List<String> UnitList = new ArrayList();
-         UnitList.add("mi");
-         UnitList.add("km");
+        List<String> UnitList = new ArrayList();
+        UnitList.add("mi");
+        UnitList.add("km");
         UnitList.add("m");
-        UnitList.add("cm"); 
-return UnitList;
+        UnitList.add("cm");
+        return UnitList;
     }
 }
